ef6c6885b7f72e87e9324b5d2fa7352c97ce2868
chris <chris@strandz.org>
2015-08-17 17:05:12 +1000

    Now all going outwards as they supposed to

--- src/quil_site/flow_gases.clj
+++ src/quil_site/flow_gases.clj
@@ -31,28 +31,52 @@
 
 (def width 500)
 (def height 500)
+(def centre [(/ width 2) (/ height 2)])
+
+(defn x-val [vec] (first vec))
+(defn y-val [vec] (second vec))
+
+(def hatchery-size 25)
+
+(defn vector->radians
+  [[x y]]
+  (. Math (atan2 y x)))
+
+(defn translate-v2
+  ([[x y] [dx dy]] (translate-v2 [x y] [dx dy] +))
+  ([[x y] [dx dy] op] [(op x dx) (op y dy)]))
+
+(defn abs
+  [[x y]] [(max x (- x)) (max y (- y))])
+
+(defn calc-direction [from-zero-vec]
+  (let [delta (translate-v2 [0 0] from-zero-vec +)
+        dir (vector->radians delta)]
+    ;(println (str "Centre is " centre ", and vec is " vec ", so delta is " delta))
+    dir))
 
 ;; If one every fps is too much we can be random
 ;; If need more then we won't use conj but concat(?), and return a vector here
 (defn create-molecule-ball []
-  {:pos [(+ (/ width 2) (rand-between -3 3))
-         (+ (/ height 2) (rand-between -3 3))]
-   :dir (rand q/TWO-PI)
-   :z 1.0
-   :col [(rand-between 150 255)
-         (rand-between 100 200)
-         (rand-between 0 100)]
-   :speed 0.1
-   :render-fn render-molecule-ball})
+  (let [x-random (rand-between (- hatchery-size) hatchery-size)
+        y-random (rand-between (- hatchery-size) hatchery-size)
+        dir (calc-direction [x-random y-random])
+        pos [(+ (x-val centre) x-random)
+             (+ (y-val centre) y-random)]]
+    {:pos pos
+     :dir dir
+     :z 1.0
+     :col [(rand-between 150 255)
+           (rand-between 100 200)
+           (rand-between 0 100)]
+     :speed 0.1
+     :render-fn render-molecule-ball}))
 
 ;; Calling particles even although max is one
 (defn emit-molecule-particles [state]
   (update-in state [:molecule-particles] conj (create-molecule-ball))
   ;(println (str "S/have added a ball, count is " (count (:molecule-particles state))))
   )
-
-(defn translate-v2 [[x y] [dx dy]]
-  [(+ x dx) (+ y dy)])
 
 (defn radians->vector
   [angle constant]
